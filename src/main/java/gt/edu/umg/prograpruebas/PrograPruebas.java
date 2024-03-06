adbf3f4e1b8146ea78454c8ee3d2a6987318ca7a
rober <rober@DESKTOP-UJS9TFF>
2024-03-06 00:18:52 -0600

    Progra 3 Menu

--- src/main/java/gt/edu/umg/prograpruebas/PrograPruebas.java
+++ src/main/java/gt/edu/umg/prograpruebas/PrograPruebas.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package gt.edu.umg.prograpruebas;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +13,35 @@ package gt.edu.umg.prograpruebas;
 public class PrograPruebas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int opc = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+
+            System.out.println("1. Arboles binarios");
+            System.out.println("2. XX");
+            System.out.println("3. YY");
+            System.out.println("4. Salir");
+
+            try {
+                opc = sc.nextInt();
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+
+                System.out.println("Se esperaba una entrada numerica.");
+                sc.nextLine();
+            } catch (Exception e) {
+
+                System.out.println("Ha ocurrido una excepción.");
+            } finally {
+               
+                
+            }
+
+        } while (opc != 4);
+        {
+            System.out.println("Prueba finalizada");
+
+        }
+
     }
 }
