f312af80fe0addc7eee9fa3afbc5995db578f521
BryanMollinedo <bmollinedoc1@miumg.edu.gt>
2024-04-30 23:36:23 -0600

    Se agregan 2 clases con los metodos de Rotación

---
+++ src/main/java/gt/edu/umg/arbolBB/ArbolAVL.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gt.edu.umg.arbolBB;
+
+/**
+ *
+ * @author Bryan
+ */
+public class ArbolAVL extends Nodo{
+
+    public ArbolAVL(int dato, Nodo izq, Nodo der) {
+        super(dato, izq, der);
+        
+        
+    }
+
+    
+}
